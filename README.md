6dfe54962a216068b25607eedc605596b7c1a68d
Maximilian Burger <8099@htl.rennweg.at>
2022-06-30 23:34:38 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# RegShotToPs
+ Simple Converter from regshot diff to Powershell reistry commands
---
+++ src/main.java
@@ -0,0 +1,44 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.LinkedList;
+
+public class main {
+    public static void main(String[] args) {
+        try (BufferedReader br = Files.newBufferedReader(Paths.get("rsc/file.txt"))) {
+            String line = "";
+
+            LinkedList<String> knownPaths = new LinkedList<>();
+
+            while ((line = br.readLine()) != null) {
+                String psString = "New-ItemProperty -Path ";
+                String type = "";
+                line = line.substring(0, 4) + "§" + line.substring(4);
+                String path = line.substring(0, line.indexOf(':')).replace(':', ' ').trim();
+                String literalPath = path.substring(0, path.lastIndexOf("\\"));
+                if (!knownPaths.contains(literalPath)) {
+                    knownPaths.add(literalPath);
+                    System.out.println("New-Item -Path \"" + literalPath.replace("§", ":") + "\" -ItemType Directory -Force");
+                }
+                psString += "\"" + literalPath + "\" -PropertyType ";
+                String value = line.substring(line.indexOf(':')).trim().replace(":", "").trim();
+                char first = value.toCharArray()[0];
+                String name = path.substring(path.lastIndexOf("\\")).replace('\\', ' ').trim();
+                if (first == '\"') {
+                    type = "String";
+                } else {
+                    type = "DWord";
+                }
+                psString += "\"" + type + "\"" + " -Name ";
+                psString += "\"" + name + "\" -Value " + value;
+
+                System.out.println(psString.replace("§", ":"));
+
+
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
